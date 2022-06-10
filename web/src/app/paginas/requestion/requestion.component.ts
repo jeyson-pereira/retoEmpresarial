e76f143c5500f51527b46c6ac5030eadae283f25
Willian Franco <ing.wfranco@gmail.com>
2022-06-10 12:25:41 -0500

    add scroll answers
    se adiciona el scroll para visaulizar las respuestas a una pregunta, segun se realiza scroll sobre el componente

--- web/src/app/paginas/requestion/requestion.component.ts
+++ web/src/app/paginas/requestion/requestion.component.ts
@@ -9,69 +9,58 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-requestion',
   templateUrl: './requestion.component.html',
-  styleUrls: ['./requestion.component.css']
+  styleUrls: ['./requestion.component.css'],
 })
 export class RequestionComponent implements OnInit {
-  
-  question:QuestionI | undefined;
+  question: QuestionI | undefined;
   answers: AnswerI[] | undefined;
-  answersNew: AnswerI[]=[];
-  currentAnswer:number=0;
+  answersNew: AnswerI[] = [];
+  currentAnswer: number = 0;
 
   questions: QuestionI[] | undefined;
- 
+
   page: number = 0;
 
   constructor(
-    private route:ActivatedRoute,
-    private questionService:QuestionService,
+    private route: ActivatedRoute,
+    private questionService: QuestionService,
     private service: QuestionService,
     public authService: ServiceService,
     private router: Router
+  ) {}
 
-    ) {
-
-    }
-
-  id:string | undefined;
+  id: string | undefined;
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.getQuestions(`${id}`);
     this.get2();
-    
   }
-  
-  get2(){
-    let id = this.route.snapshot.paramMap.get('id');
-    
 
-    this.service.getAnswer(id).subscribe((data) => {  
-          this.answers = data.answers;
+  get2() {
+    let id = this.route.snapshot.paramMap.get('id');
+
+    this.service.getAnswer(id).subscribe((data) => {
+      this.answers = data.answers;
     });
   }
 
-  getQuestions(id:string):void{
-    this.questionService.getQuestion(id).subscribe(data=>{
-      this.question=data;
+  getQuestions(id: string): void {
+    this.questionService.getQuestion(id).subscribe((data) => {
+      this.question = data;
       this.answers = data.answers;
-    })
-
+    });
   }
 
-  AddAnwsers(index:number) {
-    let last=this.currentAnswer+index;
-    for(let i = this.currentAnswer;i<last;i++){
-    }
-    this.currentAnswer+=10;
+  AddAnwsers(index: number) {
+    let last = this.currentAnswer + index;
+    for (let i = this.currentAnswer; i < last; i++) {}
+    this.currentAnswer += 10;
   }
 
-  onScroll() {
-
-  }
+  onScroll() {}
 
   preguntasHome() {
     this.router.navigate(['preguntas']);
   }
-
 }
